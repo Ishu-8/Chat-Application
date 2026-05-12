dd0f459a54a55a695e442331013ae920cc170359
Ishu-8 <ishwaryabalamurugan2004@gmail.com>
2026-05-12 16:42:22 +0530

    file updated

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-let socket = io('http://localhost:3000');
+let socket = io('https://chat-application-av19.onrender.com');
 
 let user = localStorage.getItem('user')
 if (!user) { document.location.href = 'index.html' }
